760fa100948b2d4cd8f456d4a20c2f4a43d5aa92
Team 811 <team811cardinals@gmail.com>
2023-01-17 19:25:21 -0500

    Can swerve drive manually - Alex

--- src/main/java/frc/robot/commands/DrivingCommand.java
+++ src/main/java/frc/robot/commands/DrivingCommand.java
@@ -8,6 +8,7 @@ import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
 
 public class DrivingCommand extends CommandBase {
@@ -16,7 +17,14 @@ public class DrivingCommand extends CommandBase {
   DoubleSupplier myJoyY;
   DoubleSupplier myJoyX2;
 
-  /** Creates a new DrivingCommand. */
+  /**
+   * Command to swerve drive.
+   * 
+   * @param drivetrain a drivetrain object
+   * @param joyX supplier for the left joystick X
+   * @param joyY supplier for the left joystick Y
+   * @param joyX2 supplier for the right joystick X
+   */
   public DrivingCommand(Drivetrain drivetrain, DoubleSupplier joyX, DoubleSupplier joyY, DoubleSupplier joyX2) {
     myDrivetrain = drivetrain;
     myJoyX = joyX;
@@ -29,23 +37,36 @@ public class DrivingCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
-
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(myJoyX.getAsDouble() > 0.5) {
-      double xSpeed = Constants.OperatorConstants.driveSpeedScale * myJoyX.getAsDouble();
-      double ySpeed = Constants.OperatorConstants.driveSpeedScale * myJoyY.getAsDouble();
-      double radSpeed = Constants.OperatorConstants.rotationSpeedScale * myJoyX2.getAsDouble();
-      myDrivetrain.driveWithMisery(xSpeed, ySpeed, radSpeed);
+    if(!(RobotContainer.driveController.getXButton() || RobotContainer.driveController.getYButton())){
+      if(!(myJoyX.getAsDouble() > -0.5 && myJoyX.getAsDouble() < 0.5 && myJoyY.getAsDouble() > -0.5 && myJoyY.getAsDouble() < 0.5 && myJoyX2.getAsDouble() > -0.5 && myJoyX2.getAsDouble() < 0.5)) {
+        double xSpeed = Constants.OperatorConstants.driveSpeedScale * myJoyX.getAsDouble() * Constants.OperatorConstants.maxSpeed;
+        double ySpeed = Constants.OperatorConstants.driveSpeedScale * myJoyY.getAsDouble() * Constants.OperatorConstants.maxSpeed;
+        double radSpeed = Constants.OperatorConstants.rotationSpeedScale * myJoyX2.getAsDouble() * Constants.OperatorConstants.driveSpeedScale;
+        myDrivetrain.driveWithMisery(ySpeed, xSpeed, radSpeed);
+      } else {
+        // myDrivetrain.zeroMotors();
+        myDrivetrain.driveWithMisery(0, 0, 0);
+      }    
+    }else if(RobotContainer.driveController.getXButton()) {
+      myDrivetrain.resetGyro();
     } else {
-      myDrivetrain.driveWithMisery(0, 0, 0);
+      if(!(myJoyX.getAsDouble() > -0.5 && myJoyX.getAsDouble() < 0.5 && myJoyY.getAsDouble() > -0.5 && myJoyY.getAsDouble() < 0.5 && myJoyX2.getAsDouble() > -0.5 && myJoyX2.getAsDouble() < 0.5)) {
+        double xSpeed = Constants.OperatorConstants.driveSpeedScale * myJoyX.getAsDouble() * Constants.OperatorConstants.maxSpeed;
+        double ySpeed = Constants.OperatorConstants.driveSpeedScale * myJoyY.getAsDouble() * Constants.OperatorConstants.maxSpeed;
+        double radSpeed = Constants.OperatorConstants.rotationSpeedScale * myJoyX2.getAsDouble() * Constants.OperatorConstants.driveSpeedScale;
+        double autoAim = 0;
+        myDrivetrain.driveWithMisery(ySpeed, xSpeed, radSpeed+autoAim);
+      }
     }
   }
-
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    myDrivetrain.driveWithMisery(0, 0, 0);
+  }
 
   // Returns true when the command should end.
   @Override
---
+++ src/main/java/frc/robot/subsystems/AprilTagReader.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import org.photonvision.PhotonCamera;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AprilTagReader extends SubsystemBase {
+  
+  static PhotonCamera camera = new PhotonCamera("Team811ObjectCamera");
+
+  static private NetworkTable table = NetworkTableInstance.getDefault().getTable("photonvision/Team811ObjectCamera");
+  static private NetworkTableEntry tx = table.getEntry("targetYaw");
+  static private NetworkTableEntry ty = table.getEntry("targetPitch");
+  static private NetworkTableEntry ta = table.getEntry("targetArea");
+
+  static private double x = tx.getDouble(0.0);
+  static private double y = ty.getDouble(0.0);
+  static private double area = ta.getDouble(0.0);
+
+
+  public static double getX() {
+      tx = table.getEntry("targetYaw");
+      x = tx.getDouble(0.0);
+      return x;
+  }
+
+  public static double getY() {
+      ty = table.getEntry("targetPitch");
+      y = ty.getDouble(0.0);
+      return y;
+  }
+
+  public static double getA() {
+      ta = table.getEntry("targetArea");
+      area = ta.getDouble(0.0);
+      return area;
+  }
+
+  public static boolean getV() {
+      tx = table.getEntry("targetYaw");
+      x = tx.getDouble(0.0);
+      ty = table.getEntry("targetPitch");
+      y = ty.getDouble(0.0);
+      if (x != 0 || y != 0)
+          return true;
+      else 
+          return false;    
+  }
+  /**
+   * 
+   * @return
+   */
+  public static double aim(){
+    return 0;
+  }
+
+  /** Creates a new AprilTagReader. */
+  public AprilTagReader() {
+    camera.setPipelineIndex(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    
+
+  }
+}
