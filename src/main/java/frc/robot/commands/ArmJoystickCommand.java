9c50f17fdfda7f0bc260239d6eaed16c69023819
Team 811 <team811cardinals@gmail.com>
2023-02-25 13:31:54 -0500

    manual auto added - Alex

--- src/main/java/frc/robot/commands/ArmJoystickCommand.java
+++ src/main/java/frc/robot/commands/ArmJoystickCommand.java
@@ -6,7 +6,10 @@ package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.Arm;
 
 public class ArmJoystickCommand extends CommandBase {
@@ -33,7 +36,9 @@ public class ArmJoystickCommand extends CommandBase {
     else
       joyWithDeadband = joyInput.getAsDouble();
     arm.armWithMisery(joyWithDeadband);
+  
   }
+  
 
   // Called once the command ends or is interrupted.
   @Override
